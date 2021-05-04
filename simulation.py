6af61b7df0cca1ce0f6605bc4902bc16a1d8c5ee
David Tarazi <davidtarazi@me.com>
2021-05-04 14:55:27 -0700

    simulation working, root locus plot and figures saved

--- simulation.py
+++ simulation.py
@@ -11,32 +11,28 @@ R = 10                    # solenoid resistance [Ohms]
 L = 0.05                  # solenoid length of coil [m]
 m = 0.05                  # permanent magnet mass [kg]
 g = 9.8                   # acceleration due to gravity [m^2]
-i = 10/R                   # current through electromagnet [A]
+i = 10/R                  # current through electromagnet [A]
 A = .01                   # Cross-sectional Area of Electromagnet Plunger [m^2]
 P1 = mu_0*(N*A*i)/L       # Electromagnet Flux [Wb]
-P2 = .0005                 # Permanent Magnet Flux [Wb]
-Xout = 0                  # Output distance between magnet and x_0 [m]
-Vin = 0                   # Difference in Voltage from v_0 [V]
-Vh = 0                    # Measured Voltage from Hall-Effect Sensor [V]
-
+P2 = .005                 # Permanent Magnet Flux [Wb]
 
 # Constant Defs
-x_0 = 0.03                # distance of levitation [m]
+x_0 = -0.03               # distance of levitation [m]
+ground = -0.2             # distance to ground [m] 
 
 # finding v_0      
 K_nov =  ((P2*N*A) / (4*np.pi*L*R)) * (1/(x_0**2))
 Kv = (P2*N*A) / (4*np.pi*L*R) * (1/(x_0**2))
-Kx_nov = ((P2*N*A) / (4*np.pi*L*R)) * (-2/(x_0**3))
-v_0 = (-m*g) / (x_0*Kx_nov + Kv - K_nov)          # voltage at distance of levitation [V]
-print(P1)
+Kx_nov = ((P2*N*A) / (4*np.pi*L*R)) * (2/(x_0**3))        
+v_0 = (m*g) / (x_0*Kx_nov - Kv + K_nov)   
 
 # plug v_0 back in 
 K = (P2*N*A) / (4*np.pi*L*R) * (v_0/(x_0**2))
-Kx = (P2*N*A) / (4*np.pi*L*R) * (-2*v_0/(x_0**3))
+Kx = (P2*N*A) / (4*np.pi*L*R) * (2*v_0/(x_0**3))
 
 #Force Calculations
 i = v_0/R
-P1 = mu_0*((N*A*i)/L)
+P1 = (mu_0*((N*A*i)/L))
 #F_m = (1/(mu_0*4*np.pi))*((P1*P2)/(x_0**2))
 x = x_0
 v = v_0
@@ -44,25 +40,30 @@ F_m = 2*(((P2*N*A*v_0)/(4*np.pi*L*R*(x_0**2)))-((2*v_0*P2*N*A)/((x_0**3)*4*np.pi
 F_ext = F_m-(m*g)
 print(f"Gravity Force: {m*g}")
 print(f"Magnetic Force: {F_m}")
-print(f"External Force: {F_ext}")
+print(f"Sum of Forces: {F_ext}")
 print(f"v_0: {v_0}")
 
 
-# whatever
-L = control.TransferFunction((Kv), (1, 0, -Kx))
+# Root Locus
+L = control.TransferFunction((Kv), (m, 0, Kx))
 print(L)
 rlist, klist = control.root_locus(L) 
 plt.show()
 
-# Uncontrolled TF
-G = signal.TransferFunction([Kv], [m, 0, -Kx])
-print(G)
-t, x = signal.step(G)
-
+# Uncontrolled System Simulation
+step_vs = [-3, -1, -0.1, 0, 0.1, 1, 3]
 plt.figure()
-plt.plot(t, x, "b", label='Transfer Function Solution')
+for v in step_vs:
+    G = signal.TransferFunction([v*Kv], [m, 0, Kx])
+    t, out = signal.step(G)
+    out[out > -x_0] = -x_0
+    out[out < ground] = ground
+    plt.plot(t, out, label=f'v_rel={v}')
+
+plt.axhline(y=ground, color='k', linestyle='--', label='ground')
+plt.axhline(y=-x_0, color='k', linestyle='--', label='electromagnet')
 plt.xlabel("Time [s]")
 plt.ylabel("Distance [m]")
-plt.title('title')
+plt.title('Magnet Distance from Stable Position with Variable Starting Positions')
 plt.legend()
 plt.show()
